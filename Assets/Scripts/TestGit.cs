64fb7726362a3d5df2dc20b690e7d082528aab91
JDM0625 <zzang33pp@gmail.com>
2023-08-22 20:32:22 +0900

    내용숮넝

--- Assets/Scripts/TestGit.cs
+++ Assets/Scripts/TestGit.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class TestGit : MonoBehaviour
 {
+    public int TestNo = 0;
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
